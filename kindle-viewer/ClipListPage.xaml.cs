d3fa248527b39335358b4aa80e72809b4dadcbb4
AnnatarHe <iamhele1994@gmail.com>
2018-06-25 00:09:33 +0800

    feat(viewr): add viewer

---
+++ kindle-viewer/ClipListPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Diagnostics;
+
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace kindle_viewer
+{
+    class ClipItem
+    {
+        public string title { get; set; }
+        public DateTime createdAt { get; set; }
+        public string content { get; set; }
+        public string location { get; set; }
+        public string author { get; set; }
+
+        public string toString() => $"{title}\n{content}\n{location}\n{author}\n{createdAt.ToString()}";
+    }
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ClipListPage : Page
+    {
+
+        private ObservableCollection<ClipItem> Collections = new ObservableCollection<ClipItem>();
+
+        public ClipListPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            var list = (List<ClipItem>)e.Parameter;
+
+            foreach(var item in list)
+            {
+                Debug.WriteLine(item.title);
+                this.Collections.Add(item);
+            }
+
+            Debug.WriteLine(this.Collections.Count);
+        }
+    }
+
+    
+}
--- kindle-viewer/MainPage.xaml.cs
+++ kindle-viewer/MainPage.xaml.cs
@@ -12,19 +12,106 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
+using System.Diagnostics;
+using Windows.Storage;
+using System.Text.RegularExpressions;
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace kindle_viewer
 {
+
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class MainPage : Page
     {
+
+
         public MainPage()
         {
             this.InitializeComponent();
+
+        }
+
+        private void fakeNav()
+        {
+            List<ClipItem> clipItems = new List<ClipItem>();
+
+            foreach (var i in Enumerable.Range(0, 25))
+            {
+                var item = new ClipItem();
+                item.content = $"lallalalal--{i}";
+                item.author = "AnnatarHe";
+                item.title = "C# in depth";
+
+                clipItems.Add(item);
+            }
+
+            Frame.Navigate(typeof(ClipListPage), clipItems);
+
+        }
+
+        private async void Grid_Drop(object sender, DragEventArgs e)
+        {
+            List<ClipItem> clipItems = new List<ClipItem>();
+            var storageItems = await e.DataView.GetStorageItemsAsync();
+            StorageFile file = storageItems.First() as StorageFile;
+
+
+            using (var inputStream = await file.OpenReadAsync())
+            using (var classicStream = inputStream.AsStreamForRead())
+            using (var streamReader = new StreamReader(classicStream))
+            {
+                int currentLine = 1;
+                ClipItem clipItem = new ClipItem();
+              
+                while (streamReader.Peek() >= 0)
+                {
+                    var line = streamReader.ReadLine();
+                    switch (currentLine % 5)
+                    {
+                        case 1:
+                            clipItem = new ClipItem();
+                            var result = line.Split(' ');
+                            clipItem.title = result.First();
+                            clipItem.author = result.Last();
+                            break;
+                        case 2:
+                            var locationRegex = new Regex(@"Location (\d+-\d+)");
+                            clipItem.location = locationRegex.Match(line).Value;
+                            var dateTimeRegex = new Regex(@"(20.*) [A|P]M");
+                            var thatTimeString = dateTimeRegex.Match(line).Value;
+                            Debug.WriteLine(thatTimeString);
+                            // clipItem.createdAt = DateTime.Parse(thatTimeString);
+                            break;
+                        case 4:
+                            clipItem.content = line;
+                            break;
+                        case 0:
+                            clipItems.Add(clipItem);
+                            break;
+                    }
+                    currentLine++;
+                }
+            }
+
+            var item = clipItems.ElementAt(3);
+            // Debug.WriteLine(item.content, item.title, item.author, item.location);
+            Debug.WriteLine(item.toString());
+
+            var f = Window.Current.Content as Frame;
+            f.Navigate(typeof(ClipListPage), clipItems);
+
+        }
+
+        private void Grid_DragOver(object sender, DragEventArgs e)
+        {
+            e.AcceptedOperation = Windows.ApplicationModel.DataTransfer.DataPackageOperation.Copy;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.fakeNav();
         }
     }
 }
