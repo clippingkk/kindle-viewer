b65f595abbc2b9fd7d847a5ab614db9cdd89d562
AnnatarHe <iamhele1994@gmail.com>
2018-08-05 10:03:52 +0800

    fix(db): restore db

---
+++ ClippingKKModel/ClippingKKModel/Clipping.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace ClippingKKModel
+{
+    public class ClippingContext: DbContext
+    {
+        public DbSet<ClippingItem> Clippings { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=clippingkk.db");
+        }
+
+        public void Migrate()
+        {
+            this.Database.Migrate();
+        }
+    }
+
+    public class ClippingItem
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Content { get; set; }
+        public string Location { get; set; }
+        public string Author { get; set; }
+    }
+}
---
+++ ClippingKKModel/ClippingKKModel/Migrations/20180804034552_MyFirstMigration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ClippingKKModel.Migrations
+{
+    public partial class MyFirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clippings",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(nullable: true),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    Content = table.Column<string>(nullable: true),
+                    Location = table.Column<string>(nullable: true),
+                    Author = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clippings", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clippings");
+        }
+    }
+}
