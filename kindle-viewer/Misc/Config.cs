12b1bc47808bbd62bfceeb354d256bbd8c1d0f01
AnnatarHe <iamhele1994@gmail.com>
2018-09-12 23:45:48 +0800

    fix(config): fix config

--- kindle-viewer/Misc/Config.cs
+++ kindle-viewer/Misc/Config.cs
@@ -10,15 +10,21 @@ namespace kindle_viewer.Misc
     {
         public static string UrlPrefix {
             get {
-#if DEBUG
-                return "http://api.clippingkk.annatarhe.com/api";
-                // return "http://localhost:9654/api";
+#if !DEBUG
+                // return "http://api.clippingkk.annatarhe.com/api";
+                return "http://localhost:9654/api";
 #else
-                return "http://api.clippingkk.annatarhe.com/api";
+                return "https://api.clippingkk.annatarhe.com/api";
 #endif
             }
         }
 
         public static string JWT { get; set; }
+
+        public static EasyHttp.Http.HttpClient GetHttpClient() {
+            EasyHttp.Http.HttpClient http = new EasyHttp.Http.HttpClient(Config.UrlPrefix);
+            http.Request.AddExtraHeader("jwt-token", Config.JWT);
+            return http;
+        }
     }
 }
